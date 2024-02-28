aaba1df536e28baa317a588d5e2a41df26f05541
jenavieve-layosa <jlayosa@nevada.unr.edu>
2024-02-28 09:48:31 -0800

    monaco code editor

--- package/src/views/pages/Simulator.jsx
+++ package/src/views/pages/Simulator.jsx
@@ -1,15 +1,21 @@
 import React from "react";
-
+import MonacoEditor from 'react-monaco-editor';
 import PropTypes from "prop-types";
-
-
 import Header from "../../components/header/header.jsx";
 import HeaderBanner from "../../components/banner/banner.jsx";
 import Footer from "../../components/footer/footer.jsx";
-
 import img3 from '../../assets/images/img3.jpg';
 
 const Simulator = () => {
+    const editorOptions = {
+        theme: 'hc-black', // Change the theme
+        fontSize: 16, // Change the font size
+        language: 'python', // Specify the language mode
+        lineNumbers: 'on', // Enable line numbers
+        minimap: {
+          enabled: false, // Disable minimap
+        },
+      };
     return (
         <div id="main-wrapper">
             <Header />
@@ -17,15 +23,19 @@ const Simulator = () => {
                 <div className="container-fluid text-center">
                     <HeaderBanner />
                     <h1 className="title"> Welcome to the Simulator</h1>
-                    <h2 className="subtitle">This website uses *** to create an interactive virtual environment to help demonstrate robotics concepts without the need for hardware</h2>
+                    <h2 className="subtitle">This website uses Monaco Code Editor and Gazebo to create an interactive virtual environment to help demonstrate robotics concepts without the need for hardware</h2>
 
                     <div className="intro-content">
-                        <p>
-                            *insert simulation here* 
-                        </p>
-                        <ol>                                
+                    <MonacoEditor
+                        width="800" // Set the width
+                        height="600" // Set the height
+                        options={editorOptions}
+                        onChange={(value, event) => {
+                            console.log(value);
+                        }}
+                    />
+                        
                         <img src={img3} alt="Advanced Robotics Concepts" className="img-thumbnail" width="290" />
-                        </ol>
                     </div>
                 </div>
             </div>
