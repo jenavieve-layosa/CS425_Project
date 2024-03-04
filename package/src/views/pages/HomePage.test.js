b7549be33a9c3f7f289b995fa36814004f4c67c7
jenavieve-layosa <jlayosa@nevada.unr.edu>
2024-03-04 12:17:09 -0800

    testing sucks

--- package/src/views/pages/HomePage.test.js
+++ package/src/views/pages/HomePage.test.js
@@ -1,4 +1,4 @@
-import '@testing-library/jest-dom';
+
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import HomePage from './HomePage';
