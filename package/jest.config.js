6b18c0880793b4e77614b56f30def39ed8c01522
jenavieve-layosa <jlayosa@nevada.unr.edu>
2024-03-04 11:17:50 -0800

    jest

--- package/jest.config.js
+++ package/jest.config.js
@@ -1,8 +1,43 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest',
-    },
-  };
+  // Indicates that the root directory of the project is the current directory
+  rootDir: '.',
+
+  // An array of file extensions your modules use
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+
+  // The test environment that will be used for testing
+  testEnvironment: 'jsdom',
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+
+  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  testPathIgnorePatterns: ['/node_modules/'],
+
+  // An array of directory names to be searched recursively up from the requiring module's location
+  moduleDirectories: ['node_modules', 'src'],
+
+  // A list of paths to directories that Jest should use to search for files in
+  roots: ['<rootDir>/src'],
+
+  // A list of paths to directories that Jest should use to search for files in
+  testMatch: ['<rootDir>/src/**/__tests__/**/*.js?(x)', '<rootDir>/src/**/?(*.)+(spec|test).js?(x)'],
+
+  // An array of file paths or glob patterns that should be watched
+  watchPathIgnorePatterns: ['<rootDir>/node_modules/'],
+
+  // The maximum amount of workers used to run your tests. Can be specified as % or number
+  maxWorkers: '50%',
+
+  // Indicates whether each individual test should be reported during the run
+  verbose: true,
+
+ 
+  preset: 'babel-jest',
+
+
+   
   
+};
---
+++ package/navigation.test.js
@@ -0,0 +1,43 @@
+// Navigation.test.js
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router';
+import HomePage from './src/views/pages/HomePage'; 
+import Glossary from './src/views/pages/Glossary'; 
+
+describe('Navigation', () => {
+  it('should navigate to the home page', () => {
+    const history = createMemoryHistory();
+    history.push('/');
+
+    render(
+      <Router history={history}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Glossary" element={<Glossary />} />
+        </Routes>
+      </Router>
+    );
+
+    expect(screen.getByText(/FORE: Fundamentals of Robotics Education is an interactive learning website for those of a high school to undergraduate level with an interest in learning how to program robotics!/i)).toBeInTheDocument(); 
+  });
+
+  it('should navigate to the glossary page', () => {
+    const history = createMemoryHistory();
+    history.push('/Glossary');
+
+    render(
+      <Router history={history}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Glossary" element={<Glossary />} />
+        </Routes>
+      </Router>
+    );
+
+    expect(screen.getByText(/This is our hub of knowledge when it comes to robotics terms and lingo/i)).toBeInTheDocument(); 
+  });
+});
