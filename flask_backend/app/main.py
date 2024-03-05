647acedd973ea62a5023e0f00fd27ec6fab19735
Henry Dalton <hdalton@nevada.unr.edu>
2024-03-05 11:43:22 -0800

    main profile route added

--- flask_backend/app/main.py
+++ flask_backend/app/main.py
@@ -7,15 +7,12 @@ main = Blueprint('main', __name__, static_folder='CS425_Project')
 @main.route('/')
 def serve():
     return send_from_directory(main.static_folder, 'index.html')
-@login_required
-
-    #return render_template('index.html')
 
 @main.route('/profile')
 def profile():
-    return 0
-    #return render_template('profile.html', name=current_user.name)
-
-@main.route('/api/data')
-def get_data():
-    return jsonify({'data': 'Hello World!'})
+    data = {
+        'email': current_user.email,
+        'firstname': current_user.firstname,
+        'lastname': current_user.lastname,
+    }
+    return jsonify(data)
